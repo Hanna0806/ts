e9d2c68abfb2d4475285e13126d035a0d5aaa226
anna <anna.gal0806@gmail.com>
2024-09-09 21:23:14 +0300

    add typescript

---
+++ index.js
@@ -0,0 +1,4 @@
+var str = 'str';
+var body = document.querySelector('body');
+console.log(body);
+str.map();
---
+++ index.ts
@@ -0,0 +1,4 @@
+let str = 'str'
+const body = document.querySelector('body')
+console.log(body)
+str.map()
